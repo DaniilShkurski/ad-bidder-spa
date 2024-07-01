a39c828634094c637eca8581c50835165abeda6d
DaniilShkurski <daniilshkurski@gmail.com>
2024-07-02 01:05:54 +0200

    buttons initia;

---
+++ src/MyEventButton1.jsx
@@ -0,0 +1,8 @@
+export function MyEventButton1() {
+    return (
+        <button>
+            I'm a button 1
+        </button>
+    )
+}
+//todo : add onclick action to send event to kafka or websocket
---
+++ src/MyEventButton2.jsx
@@ -0,0 +1,8 @@
+export function MyEventButton2() {
+    return (
+        <button>
+            I'm a button 2
+        </button>
+    )
+}
+//todo : add onclick action to send event to kafka or websocket
